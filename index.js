16fa9f1473b580d8a8dab7f3550297b90483d364
Amit Awasthi <92320605+Awasthya@users.noreply.github.com>
2023-12-10 19:52:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ const app = express();
 const path = require('path');
 require('./DB/Conn');
 const router = require('./Router/router.js');
-const path = require('path');
 app.use(express.json())
 const PORT = process.env.PORT || 5000;
 app.use(router);
 
-
-app.use(express.static(path.join(__dirname, './FrontEnd/build')));
-
+if (process.env.NODE_ENV === "production") {
+        app.use(express.static(path.join('./FrontEnd/build')));
+}
 app.get('*',function(req,res)  {
         res.sendFile(path.join(__dirname, './FrontEnd/build/index.html'))
 })
@@ -20,7 +19,3 @@ app.get('*',function(req,res)  {
 app.listen(PORT,()=>{
         console.log('Server is Started....')
 })
-/**
- * mongoDB link : 
- mongodb+srv://Demo:<password>@cluster0.27whm.mongodb.net/?retryWrites=true&w=majority
- */
